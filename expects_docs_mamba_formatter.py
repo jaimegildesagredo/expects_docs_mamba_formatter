7ed921a6c9ca5c1fa0b6dd6c785d35e645fb902e
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2014-02-03 17:03:58 +0100

    Add some static context to some examples

--- expects_docs_mamba_formatter.py
+++ expects_docs_mamba_formatter.py
@@ -5,8 +5,79 @@ import inspect
 
 from mamba.formatters import Formatter
 
+KEY = {
+    'code': [
+    ],
+    'mappings': {
+        'code': {
+            '_.dct': "{'bar': 0, 'baz': 1}",
+            '_.str': "'My foo string'"
+        },
+        'failures': {
+            '_.dct': "{'bar': 0, 'baz': 1}",
+            '_.str': "'My foo string'"
+        }
+    }
+}
+
+PROPERTY = {
+    'code': [
+        'class Foo(object):',
+        '\tbar = 0',
+        '\tbaz = 1',
+        '',
+        'obj = Foo()',
+        ''
+    ],
+    'mappings': {
+        'code': {
+            '_.obj': 'obj'
+        },
+        'failures': {
+            '_.obj': '<Foo object at 0x7ff289cb4310>'
+        }
+    }
+}
+
 
 class RSTFormatter(Formatter):
+
+    statics = {
+        'a': PROPERTY,
+        'an': PROPERTY,
+        'be': {
+            'code': [
+                'value = 1',
+                ''
+            ],
+            'mappings': {
+                'code': {},
+                'failures': {}
+            }
+        },
+        'have': {
+            'code': [
+                "lst = ['bar', 'baz]",
+                'itr = iter(lst)',
+                ''
+            ],
+            'mappings': {
+                'code': {
+                    '_.lst': 'lst',
+                    '_.itr': 'itr'
+                },
+                'failures': {
+                    '_.lst': "['bar', 'baz']",
+                    '_.itr': "<listiterator object at 0x7ff289cb4310>"
+                }
+            }
+        },
+        'key': KEY,
+        'keys': KEY,
+        'property': PROPERTY,
+        'properties': PROPERTY
+    }
+
     def __init__(self, settings):
         self.settings = settings
         self.expectations = {}
@@ -44,14 +115,33 @@ class RSTFormatter(Formatter):
         for name in sorted(self.expectations.keys()):
             self.write('{}\n{}\n'.format(name, '-'*len(name)))
 
+            static = self.statics.get(name)
+
+            if static is not None:
+                for static_code in static['code']:
+                    self.code(static_code)
+
             expectation = self.expectations[name]
 
             for success in expectation['successes']:
+                if static is not None:
+                    for mapping_src, mapping_dst in static['mappings']['code'].items():
+                        success = success.replace(mapping_src, mapping_dst)
+
                 self.code(success)
 
             for failure in expectation['failures']:
-                self.code(failure[0])
-                self.failure(failure[1])
+                failure_code, failure_message = failure
+
+                if static is not None:
+                    for mapping_src, mapping_dst in static['mappings']['code'].items():
+                        failure_code = failure_code.replace(mapping_src, mapping_dst)
+
+                    for mapping_src, mapping_dst in static['mappings']['failures'].items():
+                        failure_message = failure_message.replace(mapping_src, mapping_dst)
+
+                self.code(failure_code)
+                self.failure(failure_message)
 
             self._in_code_block = False
 
